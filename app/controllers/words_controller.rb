b71cd4d14c794c35ec8f46d148e560497f0114c3
Rainer Hind <rainer.hind@york.ac.uk>
2018-05-23 11:43:30 +0100

    Commenting, tidying & formatting

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,46 +1,39 @@
 # frozen_string_literal: true
 
+# Controller for Word class
 class WordsController < ApplicationController
+  # Define the actions to complete before various controller actions
   before_action :set_word, only: %i[show edit update destroy]
   before_action :set_places, only: %i[index search]
   before_action :set_sort_options, only: %i[index search]
   before_action :set_sources, only: %i[index search]
+
   before_action :authenticate_user!, except: %i[index search show random]
   before_action :authenticate_admin, except: %i[index search show random]
-
-  def get_sort_order
-    sort_string = params[:sort]
-    if sort_string
-      split = sort_string.split '_'
-      sort_field = split[0]
-      sort_dir = split[1]
-      return {sort_field.to_sym => sort_dir}
-    end
-    return {text: :asc}
-  end
 
   # GET /words
   # GET /words.json
   def index
     # Uses will_paginate gem
     @words = Word
-      .order(get_sort_order)
-      .paginate(page: params[:page], per_page: 50)
+             .order(sort_order)
+             .paginate(page: params[:page], per_page: 50)
   end
 
+  # Get a random word and go to its show page
   def random
+    # Get random num between 0 and num words
     offset = rand(Word.count)
+
+    # Get word record at random offset
     @word = Word.offset(offset).first
 
     redirect_to @word
   end
-  
+
   # GET /words/1
   # GET /words/1.json
-  def show
-    @word = Word.find_by_text(params[:text])
-    get_word_data(@word)
-  end
+  def show; end
 
   # GET /words/new
   def new
@@ -58,7 +51,7 @@ class WordsController < ApplicationController
                def_text: params[:search_definition_text],
                any: params[:any]
              )
-             .order(get_sort_order)
+             .order(sort_order)
              .paginate(page: params[:page], per_page: 50)
     render 'index'
   end
@@ -111,55 +104,70 @@ class WordsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_word
-      @word = Word.find_by_text(params[:text])
-    end
-    
-    def set_places
-      @all_places = Place.all.select('name,id').order :name
-    end
 
-    def set_sort_options
-      @sort_orders = {
-        'Word (A-Z)' => 'text_asc',
-        'Word (Z-A)' => 'text_desc',
-        # 'Place (A-Z)' => 'word.places_asc',
-        # 'Place (Z-A)' => 'word.places_desc',
-        # 'Source name (A-Z)' => 'source_asc',
-        # 'Source name (Z-A)' => 'source_desc',
-        # 'Date (most recent)' => 'date_desc',
-        # 'Date (oldest)' => 'date_asc',
-      }
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_word
+    @word = Word.find_by_text(params[:text])
+  end
 
-    def set_sources
-      truncate_len = 30
-      @all_sources = {}
-      sources = SourceMaterial.all.select('original_ref,id,ref').order :original_ref
-      sources.each do |x|
-        truncated_ref = if x&.original_ref
-                          x.original_ref.truncate(truncate_len)
-                        elsif x&.ref
-                          x.ref.truncate(truncate_len)
-                        else
-                          'No reference'
-                        end
-        @all_sources[truncated_ref] = x.id
-      end
-    end
+  # Set @sort_orders to be a map of sort option text -> string representing field_sortorder (used by sort_order)
+  def set_sort_options
+    @sort_orders = {
+      'Word (A-Z)' => 'text_asc',
+      'Word (Z-A)' => 'text_desc',
+      # 'Place (A-Z)' => 'word.places_asc',
+      # 'Place (Z-A)' => 'word.places_desc',
+      # 'Source name (A-Z)' => 'source_asc',
+      # 'Source name (Z-A)' => 'source_desc',
+      # 'Date (most recent)' => 'date_desc',
+      # 'Date (oldest)' => 'date_asc',
+    }
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def word_params
-      params.require(:word).permit(:word)
+  # Retrieve the 'sort' param, and split it to get the sort field and direction
+  def sort_order
+    sort_string = params[:sort]
+    if sort_string
+      split = sort_string.split '_'
+      sort_field = split[0]
+      sort_dir = split[1]
+      return {sort_field.to_sym => sort_dir}
     end
+    return {text: :asc}
+  end
 
-    def authenticate_admin
-      flash[:error] = "You're not authenticated to access that page."
-      redirect_to root_path unless current_user.admin? 
+  # Set @all_sources to be a map of truncate(original_ref) -> id, used by the sources select list of the search view
+  def set_sources
+    truncate_len = 30
+    @all_sources = {}
+    sources = SourceMaterial.all.select('original_ref,id,ref').order :original_ref
+    sources.each do |x|
+      truncated_ref = if x&.original_ref
+                        x.original_ref.truncate(truncate_len)
+                      elsif x&.ref
+                        x.ref.truncate(truncate_len)
+                      else
+                        'No reference'
+                      end
+      @all_sources[truncated_ref] = x.id
     end
+  end
 
-    def get_word_data(word)
-      @defs = word.definitions
-    end
+  # Set @all_places to be used by the select list in the search view
+  def set_places
+    @all_places = Place.all.select('name,id').order :name
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def word_params
+    params.require(:word).permit(:word)
+  end
+
+  # Check if user is an admin and error/redirect if not
+  def authenticate_admin
+    return if current_user.admin?
+
+    flash[:error] = "You're not authenticated to access that page."
+    redirect_to root_path
+  end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
+# Word class, which has many definitions
 class Word < ApplicationRecord
   has_many :definitions
 
   has_many :definition_sources, through: :definitions
   has_many :places, through: :definition_sources
   has_many :source_materials, through: :definition_sources
-  
+
+  # Use the Word text as the param for the URL
+  # https://apidock.com/rails/ActiveRecord/Base/to_param
   def to_param
     text
   end
 
+  # Search for words
   def self.search(search)
     if search
+      # Pull out individual params from the search parameter
       text = search[:text]
       places = search[:places]
       source_ids = search[:source_material_ids]
@@ -20,50 +25,59 @@ class Word < ApplicationRecord
       letter = search[:letter]
       any = search[:any]
 
+      # Build a join on the tables of interest
       query = joins(:definitions, :places, :source_materials)
+      
+      # Clean up 2 array params, as they can equal [''], which should be classed as empty
       source_ids = check_empty_search_arrays source_ids
       places = check_empty_search_arrays places
 
+      # Freeform query (searches any field of interest)
       if any.present?
         query.where!(
-          'definitions.text ILIKE :any OR definitions.discussion ILIKE :any OR words.text ILIKE :any OR places.name LIKE :any OR source_materials.original_ref ILIKE :any', any: "%#{any}%"
+          'definitions.text ILIKE :any
+          OR definitions.discussion ILIKE :any
+          OR words.text ILIKE :any
+          OR places.name LIKE :any
+          OR source_materials.original_ref ILIKE :any',
+          any: "%#{any}%"
         )
       end
 
+      # Search definition text
       if def_text.present?
-        query.where!('definitions.text ILIKE \'%%%s%%\'', def_text)
-              .or!(query.where('definitions.discussion ILIKE \'%%%s%%\'', def_text))
+        query
+          .where!('definitions.text ILIKE \'%%%s%%\'', def_text)
+          .or!(
+            query.where('definitions.discussion ILIKE \'%%%s%%\'', def_text)
+          )
       end
 
-      if letter.present?
-        query.where!('words.text ILIKE \'%s%%\'', letter)
-      end
+      # Search by first letter
+      query.where!('words.text ILIKE \'%s%%\'', letter) if letter.present?
 
-      if text.present?
-        query.where!('words.text ILIKE \'%%%s%%\'', text)
-      end
+      # Search by  word text
+      query.where!('words.text ILIKE \'%%%s%%\'', text) if text.present?
 
-      if places&.present?
-        query.where!("places.id IN (?)", places)
-      end
+      # Search by place ID
+      query.where!('places.id IN (?)', places) if places&.present?
 
-      if source_ids&.present?
-        query.where!('source_materials.id in (?)', source_ids)
-      end
+      # Search by source IDs
+      query.where!('source_materials.id in (?)', source_ids) if source_ids&.present?
 
+      # Only return each word once
       results = query.distinct
     else
+      # If no search specified, return all words
       results = all
     end
-    results
+    return results
   end
 
-  private
-
-    def self.check_empty_search_arrays array
-      if array&.size == 1 && array&.first == ''
-        array = nil
-      end
-      return array
+  def self.check_empty_search_arrays(array)
+    if array&.size == 1 && array&.first == ''
+      array = nil
     end
+    return array
+  end
 end
