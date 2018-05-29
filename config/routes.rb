68790637dfb695c19dafc9e90d904f986018ba61
Rainer Hind <rainer.hind@york.ac.uk>
2018-05-29 15:47:22 +0100

    Clean up routes to use block syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
 
-  resources :places
   devise_for :users
+  
   root 'pages#home'
-  get 'words/search', to: 'words#search'
-  get 'words/random', to: 'words#random'
-  resources :words, param: :text
+  
+  resources :words, param: :text do
+    get 'search', on: :collection
+    get 'random', on: :collection
+  end
+  
+  resources :places do
+    get 'search', on: :collection
+    get 'id_search', on: :collection
+  end
+  
+  
   get 'contact', to: 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
