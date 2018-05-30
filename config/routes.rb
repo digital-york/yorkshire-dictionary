422bc3b9e2fd753172589cc0354117aa93654cd9
Rainer Hind <rainer.hind@york.ac.uk>
2018-05-30 15:10:46 +0100

    Minor styling on words show page
    - Also add missing frozen string comment to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,25 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
   devise_for :users
-  
+
   root 'pages#home'
-  
+
   resources :words, param: :text do
     get 'search', on: :collection
     get 'random', on: :collection
   end
-  
+
   resources :places do
     get 'search', on: :collection
     get 'id_search', on: :collection
   end
-  
+
   resources :source_materials do
     get 'search', on: :collection
     get 'id_search', on: :collection
   end
-  
+
   get 'contact', to: 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
