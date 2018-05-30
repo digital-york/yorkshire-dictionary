c1a04a309cfa290be3aa079386ac353199d14717
Rainer Hind <rainer.hind@york.ac.uk>
2018-05-30 15:03:31 +0100

    Add bounds & region to geocode query in attempt to improve quality of co-ords

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,8 @@
 # Class for places, which are used by Sources
 class Place < ApplicationRecord
   # See geocoder gem @ https://github.com/alexreisner/geocoder
-  geocoded_by :name
+  # Set bounds to yorkshire and region to GB
+  geocoded_by :name, :params => {:region => "gb", :bounds => [[54.9616, 0.72532], [52.9186,-3.05396]]}
   after_validation :geocode, if: -> (obj) { obj.name.present? and obj.latitude.nil? }
 
   # Join table
