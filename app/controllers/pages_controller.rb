e1a6b85a0fc15a29f0acad0a3ee45e505be9aad3
Rainer Hind <rainer.hind@york.ac.uk>
2018-07-09 14:36:50 +0100

    Add about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
   def contact
-    render 'contact/contact'
+    render 'single_pages/contact'
   end
 
   def home
     render 'single_pages/home', layout: false
+  end
+
+  def about
+    render 'single_pages/about'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   end
 
   get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
