7a2b86e1a9abc2f1ad30b3c5f96de24e85586324
Rainer Hind <rainer.hind@york.ac.uk>
2018-07-11 16:55:08 +0100

    Use opencage instead of google for geocoding & use the result nearest to York.

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,9 +4,29 @@
 class Place < ApplicationRecord
   # See geocoder gem @ https://github.com/alexreisner/geocoder
   # Set bounds to yorkshire and region to GB
-  geocoded_by :name, params:  { region: 'gb',
-                                bounds: [[54.9616, 0.72532], [52.9186, -3.05396]],
-                                components: 'administrative_area:yorkshire'}
+  geocoded_by :name, params:  { countrycode: 'gb'} do |obj, results|
+    closest_distance = nil
+    closest_result = nil
+
+    york = { latitude: 53.95763, longitude:-1.08271 }
+
+    results.each do |result|
+      lat_difference = (york[:latitude] - result.latitude).abs
+      long_difference = (york[:longitude] - result.longitude).abs
+
+      euclidean_distance = Math.sqrt(lat_difference**2 + long_diff**2)
+      if (closest_result.nil? || euclidean_distance < closest_distance)
+        closest_distance = euclidean_distance
+        closest_result = result
+      end
+    end
+
+    if closest_result
+      obj.latitude = closest_result.latitude
+      obj.longitude = closest_result.longitude
+    end
+  end
+  
   after_validation :geocode, if: ->(place) { place.name.present? && place.latitude.nil? }
 
   # Join table
