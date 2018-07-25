093727ac0e75b14a76ce788319cba9486aeff615
Rainer Hind <rainer.hind@york.ac.uk>
2018-07-25 13:35:36 +0100

    Add geocode test

--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class PlaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'places should be geocodable' do
+    place = Place.new name: 'York, UK'
+    place.geocode
+    assert place.latitude.present?
+    assert place.longitude.present?
+  end
 end
