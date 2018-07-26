1de217c21e632fe19709589dac468f6adc10dcd6
Rainer Hind <rainer.hind@york.ac.uk>
2018-07-26 10:30:16 +0100

    Add tests on definition relations

--- test/models/definition_relation_test.rb
+++ test/models/definition_relation_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class DefinitionRelationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @defi = definitions(:one)
+    @related = definitions(:two)
+    @defi.related_definitions << @related
+  end
+
+  test 'definition relations can be accessed' do
+    assert @defi.related_definitions.include? @related
+  end
+
+  test 'inverse definition relations can be accessed' do
+    assert @related.inverse_related_definitions.include? @defi
+  end
+
+  test 'definition relations shouldn\'t include inverse relations' do
+    assert @related.related_definitions.exclude? @defi
+  end
+
+  test 'inverse definition relations shouldn\'t include regular relations' do
+    assert @defi.inverse_related_definitions.exclude? @related
+  end
 end
