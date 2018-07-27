d4383d250301e395ab828d3a9c4e2390b96cc41e
Rainer Hind <rainer.hind@york.ac.uk>
2018-07-27 14:32:46 +0100

    Test presence of map container for places

--- test/controllers/places_controller_test.rb
+++ test/controllers/places_controller_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class PlacesControllerTest < ActionDispatch::IntegrationTest
@@ -5,40 +7,50 @@ class PlacesControllerTest < ActionDispatch::IntegrationTest
     @place = places(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get places_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_place_url
     assert_response :success
   end
 
-  test "should create place" do
+  test 'should create place' do
     assert_difference('Place.count') do
-      post places_url, params: { place: {  } }
+      post places_url, params: { place: {} }
     end
 
     assert_redirected_to place_url(Place.last)
   end
 
-  test "should show place" do
+  test 'place view should contain map container' do
+    get place_url(@place)
+    assert_select '#map'
+  end
+
+  test 'index should contain map container' do
+    get places_url
+    assert_select '#map'
+  end
+
+  test 'should show place' do
     get place_url(@place)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_place_url(@place)
     assert_response :success
   end
 
-  test "should update place" do
-    patch place_url(@place), params: { place: {  } }
+  test 'should update place' do
+    patch place_url(@place), params: { place: {} }
     assert_redirected_to place_url(@place)
   end
 
-  test "should destroy place" do
+  test 'should destroy place' do
     assert_difference('Place.count', -1) do
       delete place_url(@place)
     end
