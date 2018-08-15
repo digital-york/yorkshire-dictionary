8362e724428bc97f1142c2efaf656718350914ce
Rainer Hind <rainer.hind@york.ac.uk>
2018-08-15 13:07:46 +0100

    Update submissions to include extra fields

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -3,13 +3,18 @@
 class SubmissionsController < ApplicationController
 
   def index;end
-  
+
   def create
     puts params
     SubmissionMailer
       .with(
         email_address: params[:email_address],
-        content: params[:content]
+        new: params[:new],
+        word: params[:word],
+        dates: params[:dates],
+        sources: params[:sources],
+        quotes: params[:quotes],
+        notes: params[:notes]
       )
       .submission_email.deliver_now
     flash[:notice] = 'Submission sent. Thanks for your contribution!'
--- app/mailers/submission_mailer.rb
+++ app/mailers/submission_mailer.rb
@@ -5,8 +5,13 @@ class SubmissionMailer < ActionMailer::Base
   SUBMISSION_EMAIL_ADDRESS = 'rainer.hind@york.ac.uk'
 
   def submission_email
-    @content = params[:content]
     @email_address = params[:email_address]
+    @sources = params[:sources]
+    @notes = params[:sources]
+    @dates = params[:sources]
+    @quotes = params[:sources]
+    @word = params[:word]
+    @new = params[:new]
     mail(to: SUBMISSION_EMAIL_ADDRESS, subject: 'YHD Submission Form Entry')
   end
 end
