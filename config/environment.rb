7f082eae579b060719f8d971bbe8bd5ffa52d8b5
Rainer Hind <rainer.hind@york.ac.uk>
2018-08-15 10:46:14 +0100

    Move mailer config back to environment.rb, and configure in dev/test if credentials are available

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,23 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+mailer_creds = Rails.application.credentials.mailer
+
+# Always try to configure the mailer outside test and dev envs
+# If the mailer creds are present, configure it in those two envs too
+if (!Rails.env.development? && !Rails.env.test?) || mailer_creds.present?
+  puts "Configuring mailer"
+  Rails.application.configure do
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            mailer_creds[:user_name],
+      password:             mailer_creds[:password],
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
+  end
+end
