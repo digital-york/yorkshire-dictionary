6e44f308208f72b726b9fee678043bdfc6295500
Rainer Hind <rainer.hind@york.ac.uk>
2018-08-16 15:04:56 +0100

    Add places field to submissions form, and fix bug with getting other submission field values

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -8,10 +8,11 @@ class SubmissionsController < ApplicationController
     SubmissionMailer
       .with(
         email_address: params[:email_address],
-        new: params[:new],
+        new_entry: params[:new_entry],
         word: params[:word],
         dates: params[:dates],
         sources: params[:sources],
+        places: params[:places],
         quotes: params[:quotes],
         notes: params[:notes]
       )
--- app/mailers/submission_mailer.rb
+++ app/mailers/submission_mailer.rb
@@ -7,11 +7,19 @@ class SubmissionMailer < ActionMailer::Base
   def submission_email
     @email_address = params[:email_address]
     @sources = params[:sources]
-    @notes = params[:sources]
-    @dates = params[:sources]
-    @quotes = params[:sources]
+    @notes = params[:notes]
+    @dates = params[:dates]
+    @quotes = params[:quotes]
+    @places = params[:places]
     @word = params[:word]
-    @new = params[:new]
+    new_entry_param = params[:new_entry]
+    if new_entry_param.present?
+      if new_entry_param === 'true'
+        @new_entry = true
+      else
+        @new_entry = false
+      end
+    end
     mail(to: SUBMISSION_EMAIL_ADDRESS, subject: 'YHD Submission Form Entry')
   end
 end
