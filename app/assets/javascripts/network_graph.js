f25cb1502b3e6943da050b5780300c02e2225155
Rainer Hind <rainer.hind@york.ac.uk>
2018-09-04 13:58:34 +0100

    Improve network graph
    - Improve styling & layout
    - Fix to work for pages with multiple definitions

--- app/assets/javascripts/network_graph.js
+++ app/assets/javascripts/network_graph.js
@@ -4,8 +4,8 @@
   let nodeData = [];
 
   // Get the canvas HTML element
-  function getNetworkCanvas() {
-    return document.getElementById('network-graph').getElementsByTagName('canvas')[0];
+  function getNetworkContainers() {
+    return document.querySelectorAll(".network-graph-container");
   }
 
   function nodeClick(params) {
@@ -20,83 +20,71 @@
     }
   }
 
-  function changeCursor(style) {
-    getNetworkCanvas().style.cursor = style;
+  function changeCursor(container, style) {
+    container.getElementsByTagName('canvas')[0].style.cursor = style;
   }
 
-  function createGraph(nodes, edges) {
-    // create a network
-    const container = document.getElementById('network-graph');
-
-    const options = {
-      interaction: { hover: true, hoverConnectedEdges: false },
-      nodes: {
-        font: {
-          size: 20,
-          multi: true,
-          face: 'Heebo'
-        },
-      },
-      edges: {
-        chosen: false,
-        color: {
-          color: 'black',
-          inherit: false,
-          opacity: 0.4,
-        },
-        width: 2,
-      },
-    };
+  function createGraph(container, nodes, edges) {
+    const options = { interaction: { hover: true, hoverConnectedEdges: false }, nodes: { font: { size: 20, multi: true, face: "Heebo" } }, edges: { chosen: false, color: { color: "black", inherit: false, opacity: 0.4 }, width: 2 } };
 
     const data = { nodes, edges };
 
+    const definitionId = container.dataset.definitionId;
+    const graphDiv = container.querySelector('.network-graph');
+
     // Save node data since it's used to lookup data for a node when clicked
-    nodes.forEach((node) => {
+    nodes.forEach(node => {
       nodeData[node.id] = node;
     });
 
     // initialize your network!
-    const network = new vis.Network(container, data, options);
+    const network = new vis.Network(graphDiv, data, options);
 
-    network.on('hoverNode', () => {
-      changeCursor('pointer');
+    network.on("hoverNode", () => {
+      changeCursor(graphDiv, "pointer");
     });
 
-    network.on('blurNode', () => {
-      changeCursor('default');
+    network.on("blurNode", () => {
+      changeCursor(graphDiv, "default");
     });
 
-    network.on('click', nodeClick);
+    network.on("click", nodeClick);
 
-    network.on('stabilized', () => {
-      $('#network-graph-container').show();
-      $('#network-graph-loading-message').hide();
+    network.on("stabilized", () => {
+      $(graphDiv).show();
+      $(`.network-graph-container[data-definition-id="${definitionId}"] .network-graph-loading-message`).hide();
+      network.fit();
+
     });
 
     return network;
   }
 
-  function setUpNetworkGraph() {
-    if (!$('#network-graph').length) {
+  function setUpNetworkGraphs() {
+    const containers = getNetworkContainers();
+    if (!containers.length) {
       return;
     }
+    
+    containers.forEach((container) => {
+      const definitionId = container.dataset.definitionId;
+  
+      const dataUrl = Routes.network_graph_path({ id: definitionId });
+  
+      $.getJSON({
+        url: dataUrl,
+        type: 'get',
+        data: {
+          id: definitionId,
+        },
+      }).done(
+        (data) => {
+          createGraph(container, data.nodes, data.edges);
+        },
+      );
+    });
 
-    const definitionId = $('#network-graph').data('definition-id');
-
-    const dataUrl = Routes.network_graph_path({ id: definitionId });
-
-    $.getJSON({
-      url: dataUrl,
-      type: 'get',
-      data: {
-        id: definitionId,
-      },
-    }).done(
-      (data) => {
-        createGraph(data.nodes, data.edges);
-      },
-    );
   }
 
-  $(document).on('turbolinks:load', setUpNetworkGraph);
+  $(document).on('turbolinks:load', setUpNetworkGraphs);
 }());
