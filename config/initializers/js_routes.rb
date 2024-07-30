91a703bc330909525a3fcd4553faeda3ce4fb17a
JS <jibin.samuel@york.ac.uk>
2024-07-30 23:11:45 +0100

    fix: routes

--- config/initializers/js_routes.rb
+++ config/initializers/js_routes.rb
@@ -1,5 +1,4 @@
-# config/initializers/js_routes.rb
 JsRoutes.setup do |config|
-    # You can specify the routes you want to include here, or leave it blank to include all routes.
-    # Example: config.include = [/^admin_/]
+    config.module_type = nil
+    config.namespace = 'Routes'
   end
