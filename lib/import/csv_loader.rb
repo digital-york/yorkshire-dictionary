98637a9c061c8b893f2840ace1c40efc6cad62bd
JS <jibin.samuel@york.ac.uk>
2024-08-01 17:37:09 +0100

    fix: csv loading

--- lib/import/csv_loader.rb
+++ lib/import/csv_loader.rb
@@ -14,7 +14,8 @@ class CsvLoader
       # Check if path matches file
       if csv_file_regex.match?(path)
         puts "\tFound #{filename} at #{path}. Reading..."
-        return CSV.read(path, 'r:bom|utf-8')
+        csv_content = File.read(path, mode: 'r:bom|utf-8')
+        return CSV.parse(csv_content, headers: true)
       end
     end
     puts "ERROR: Couldn't find #{filename} anywhere."
